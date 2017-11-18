5f65e60aa5a7edf9314bd40b43f6bfdbb7d0f275
Eric Mellino <mellinoe@gmail.com>
2017-11-18 14:33:30 -0800

    Update Veldrid and add a compute shader sample.

---
+++ src/ComputeParticles/ComputeParticles.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using Veldrid.Utilities;
+
+namespace ComputeParticles
+{
+    internal class ComputeParticles
+    {
+        public const int ParticleCount = 1024;
+
+        private readonly Sdl2Window _window;
+        private readonly GraphicsDevice _gd;
+        private Veldrid.Buffer _particleBuffer;
+        private Veldrid.Buffer _screenSizeBuffer;
+        private Shader _computeShader;
+        private Pipeline _computePipeline;
+        private Shader _vertexShader;
+        private Shader _fragmentShader;
+        private Pipeline _graphicsPipeline;
+        private ResourceSet _graphicsParticleResourceSet;
+        private CommandList _cl;
+        private ResourceSet _screenSizeResourceSet;
+        private ResourceSet _computeScreenSizeResourceSet;
+        private ResourceSet _computeResourceSet;
+        private bool _windowResized;
+
+        public ComputeParticles()
+        {
+            WindowCreateInfo wci = new WindowCreateInfo
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Compute Particles"
+            };
+            _window = VeldridStartup.CreateWindow(ref wci);
+            _window.Resized += () => _windowResized = true;
+
+            GraphicsDeviceCreateInfo gdci = new GraphicsDeviceCreateInfo();
+#if DEBUG
+            gdci.DebugDevice = true;
+#endif
+            _gd = VeldridStartup.CreateGraphicsDevice(ref gdci, _window);
+        }
+
+        public void Run()
+        {
+            DisposeCollectorResourceFactory factory = new DisposeCollectorResourceFactory(_gd.ResourceFactory);
+            CreateResources(factory);
+
+            while (_window.Exists)
+            {
+                _window.PumpEvents();
+
+                if (_window.Exists)
+                {
+                    Draw();
+                }
+            }
+
+            factory.DisposeCollector.DisposeAll();
+        }
+
+        private void CreateResources(ResourceFactory factory)
+        {
+            _particleBuffer = factory.CreateBuffer(
+                new BufferDescription(
+                    (uint)Unsafe.SizeOf<ParticleInfo>() * ParticleCount,
+                    BufferUsage.StructuredBufferReadWrite,
+                    false,
+                    (uint)Unsafe.SizeOf<ParticleInfo>()));
+
+            _screenSizeBuffer = factory.CreateBuffer(new BufferDescription(16, BufferUsage.UniformBuffer));
+
+            _computeShader = factory.CreateShader(new ShaderDescription(
+                ShaderStages.Compute,
+                File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Shaders", $"compute.{GetExtension(factory.BackendType)}"))));
+
+            ResourceLayout particleStorageLayout = factory.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("ParticlesBuffer", ResourceKind.StructuredBufferReadWrite, ShaderStages.Compute)));
+
+            ResourceLayout screenSizeLayout = factory.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("ScreenSizeBuffer", ResourceKind.UniformBuffer, ShaderStages.Compute)));
+
+            ComputePipelineDescription computePipelineDesc = new ComputePipelineDescription(
+                new ShaderStageDescription(ShaderStages.Compute, _computeShader, "CS"),
+                new[] { particleStorageLayout, screenSizeLayout });
+            _computePipeline = factory.CreateComputePipeline(ref computePipelineDesc);
+
+            _computeResourceSet = factory.CreateResourceSet(new ResourceSetDescription(particleStorageLayout, _particleBuffer));
+
+            _computeScreenSizeResourceSet = factory.CreateResourceSet(new ResourceSetDescription(screenSizeLayout, _screenSizeBuffer));
+
+            _vertexShader = factory.CreateShader(new ShaderDescription(
+                ShaderStages.Vertex,
+                File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Shaders", $"vertex.{GetExtension(factory.BackendType)}"))));
+            _fragmentShader = factory.CreateShader(new ShaderDescription(
+                ShaderStages.Fragment,
+                File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Shaders", $"fragment.{GetExtension(factory.BackendType)}"))));
+
+            ShaderSetDescription shaderSet = new ShaderSetDescription(
+                Array.Empty<VertexLayoutDescription>(),
+                new[]
+                {
+                    new ShaderStageDescription(ShaderStages.Vertex, _vertexShader, "VS"),
+                    new ShaderStageDescription(ShaderStages.Fragment, _fragmentShader, "FS")
+                });
+
+            particleStorageLayout = factory.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("ParticlesBuffer", ResourceKind.StructuredBufferReadOnly, ShaderStages.Vertex)));
+
+            screenSizeLayout = factory.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("ScreenSizeBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex)));
+
+            GraphicsPipelineDescription particleDrawPipelineDesc = new GraphicsPipelineDescription(
+                BlendStateDescription.SingleOverrideBlend,
+                DepthStencilStateDescription.Disabled,
+                RasterizerStateDescription.Default,
+                PrimitiveTopology.PointList,
+                shaderSet,
+                new[] { particleStorageLayout, screenSizeLayout },
+                _gd.SwapchainFramebuffer.OutputDescription);
+
+            _graphicsPipeline = factory.CreateGraphicsPipeline(ref particleDrawPipelineDesc);
+
+            _graphicsParticleResourceSet = factory.CreateResourceSet(new ResourceSetDescription(
+                particleStorageLayout,
+                _particleBuffer));
+
+            _screenSizeResourceSet = factory.CreateResourceSet(new ResourceSetDescription(
+                screenSizeLayout,
+                _screenSizeBuffer));
+
+            _cl = factory.CreateCommandList();
+
+            InitResources(factory);
+        }
+
+        private object GetExtension(GraphicsBackend backendType)
+        {
+            return backendType == GraphicsBackend.Direct3D11 ? "hlsl.bytes"
+                : backendType == GraphicsBackend.Vulkan ? "spv"
+                    : "430.glsl";
+        }
+
+        private void InitResources(ResourceFactory factory)
+        {
+            _cl.Begin();
+            _cl.UpdateBuffer(_screenSizeBuffer, 0, new Vector4(_window.Width, _window.Height, 0, 0));
+
+            ParticleInfo[] initialParticles = new ParticleInfo[ParticleCount];
+            System.Random r = new System.Random();
+            for (int i = 0; i < ParticleCount; i++)
+            {
+                ParticleInfo pi = new ParticleInfo();
+                pi.Position = new Vector2((float)(r.NextDouble() * _window.Width), (float)(r.NextDouble() * _window.Height));
+                pi.Velocity = new Vector2((float)(r.NextDouble() * 0.03), (float)(r.NextDouble() * 0.03));
+                pi.Color = new Vector4(0.4f + (float)r.NextDouble() * .6f, 0.4f + (float)r.NextDouble() * .6f, 0.4f + (float)r.NextDouble() * .6f, 1);
+                initialParticles[i] = pi;
+            }
+            _cl.UpdateBuffer(_particleBuffer, 0, initialParticles);
+
+            _cl.End();
+            _gd.ExecuteCommands(_cl);
+            _gd.WaitForIdle();
+        }
+
+        private void Draw()
+        {
+            _cl.Begin();
+            if (_windowResized)
+            {
+                _windowResized = false;
+                _gd.ResizeMainWindow((uint)_window.Width, (uint)_window.Height);
+                _cl.UpdateBuffer(_screenSizeBuffer, 0, new Vector4(_window.Width, _window.Height, 0, 0));
+            }
+
+            _cl.SetPipeline(_computePipeline);
+            _cl.SetComputeResourceSet(0, _computeResourceSet);
+            _cl.SetComputeResourceSet(1, _computeScreenSizeResourceSet);
+            _cl.Dispatch(1024, 1, 1);
+
+            _cl.SetFramebuffer(_gd.SwapchainFramebuffer);
+            _cl.SetFullViewports();
+            _cl.SetFullScissorRects();
+            _cl.ClearColorTarget(0, RgbaFloat.Black);
+            _cl.SetPipeline(_graphicsPipeline);
+            _cl.SetGraphicsResourceSet(0, _graphicsParticleResourceSet);
+            _cl.SetGraphicsResourceSet(1, _screenSizeResourceSet);
+            _cl.Draw(ParticleCount, 1, 0, 0);
+            _cl.End();
+
+            _gd.ExecuteCommands(_cl);
+            _gd.SwapBuffers();
+        }
+    }
+
+    struct ParticleInfo
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Vector4 Color;
+        public ParticleInfo(Vector2 position, Vector2 velocity, Vector4 color)
+        {
+            Position = position;
+            Velocity = velocity;
+            Color = color;
+        }
+    }
+}
---
+++ src/ComputeParticles/Shaders/Compute.450.glsl
@@ -0,0 +1,64 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+#define PARTICLE_COUNT 1024
+
+struct ParticleInfo
+{
+    vec2 Position;
+    vec2 Velocity;
+    vec4 Color;
+};
+
+layout(std140, set = 0, binding = 0) buffer ParticlesBuffer
+{
+    ParticleInfo Particles[];
+};
+
+layout(set = 1, binding = 0) uniform ScreenSizeBuffer
+{
+    float ScreenWidth;
+    float ScreenHeight;
+    vec2 Padding_;
+};
+
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+void main()
+{
+    uint index = gl_GlobalInvocationID.x;
+    if (index > PARTICLE_COUNT)
+    {
+        return;
+    }
+
+    vec2 pos = Particles[index].Position;
+    vec2 vel = Particles[index].Velocity;
+
+    vec2 newPos = pos + vel;
+    vec2 newVel = vel;
+    if (newPos.x > ScreenWidth)
+    {
+        newPos.x -= (newPos.x - ScreenWidth);
+        newVel.x *= -1;
+    }
+    if (newPos.x < 0)
+    {
+        newPos.x *= -1;
+        newVel.x *= -1;
+    }
+    if (newPos.y > ScreenHeight)
+    {
+        newPos.y -= (newPos.y - ScreenHeight);
+        newVel.y *= -1;
+    }
+    if (newPos.y < 0)
+    {
+        newPos.y = -newPos.y;
+        newVel.y *= -1;
+    }
+
+    Particles[index].Position = newPos;
+    Particles[index].Velocity = newVel;
+}
