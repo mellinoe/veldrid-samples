dd7e4c207a458ba11cb1a6404d28f7f4ea7e53bf
Gleb Lebedev <gleb@gleblebedev.com>
2020-05-03 14:59:36 +0100

    UWP sample

---
+++ src/TexturedCube/UWP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SampleBase;
+using SharpDX.DXGI;
+using Veldrid;
+using Veldrid.Utilities;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TexturedCube.UWP
+{
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private TexturedCube _sample;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _sample = new TexturedCube(_veldrid);
+        }
+
+      
+    }
+}
---
+++ src/TexturedCube/UWP/VeldridControl.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using SampleBase;
+using Veldrid;
+using Veldrid.Utilities;
+
+namespace TexturedCube.UWP
+{
+    public sealed partial class VeldridControl : SwapChainPanel, ApplicationWindow
+    {
+        private GraphicsDevice _graphicsDevice;
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        private DisposeCollectorResourceFactory _resources;
+
+        public VeldridControl()
+        {
+            InitializeComponent();
+
+            CompositionScaleChanged += OnPanelScaleChanged;
+            SizeChanged += OnPanelSizeChanged;
+
+            CompositionTarget.Rendering += RenderPanel;
+
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        public event Action<float> Rendering;
+        public event Action<GraphicsDevice, ResourceFactory, Swapchain> GraphicsDeviceCreated;
+        public event Action GraphicsDeviceDestroyed;
+        public event Action Resized;
+        public event Action<KeyEvent> KeyPressed;
+
+        public SamplePlatformType PlatformType => SamplePlatformType.Desktop;
+        uint ApplicationWindow.Width => (uint) (RenderSize.Width * CompositionScaleX);
+        uint ApplicationWindow.Height => (uint) (RenderSize.Height * CompositionScaleY);
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            DestroyDevice();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CreateDevice();
+        }
+
+
+        private void DestroyDevice()
+        {
+            if (_graphicsDevice != null)
+            {
+                GraphicsDeviceDestroyed?.Invoke();
+                _graphicsDevice.WaitForIdle();
+                _resources.DisposeCollector.DisposeAll();
+                _graphicsDevice.Dispose();
+                _graphicsDevice = null;
+            }
+        }
+
+        private void CreateDevice()
+        {
+            var options = new GraphicsDeviceOptions(true, PixelFormat.R32_Float, true, ResourceBindingModel.Improved);
+            var logicalDpi = 96.0f * CompositionScaleX;
+            var renderWidth = RenderSize.Width;
+            var renderHeight = RenderSize.Height;
+            _graphicsDevice = GraphicsDevice.CreateD3D11(options, this, renderWidth, renderHeight, logicalDpi);
+            _resources = new DisposeCollectorResourceFactory(_graphicsDevice.ResourceFactory);
+            GraphicsDeviceCreated?.Invoke(_graphicsDevice, _resources, _graphicsDevice.MainSwapchain);
+        }
+
+        private void RenderPanel(object sender, object e)
+        {
+            var seconds = (float) _stopwatch.Elapsed.TotalSeconds;
+            _stopwatch.Restart();
+            Rendering?.Invoke(seconds);
+        }
+
+        private void OnPanelSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (_graphicsDevice != null) Resized?.Invoke();
+        }
+
+        private void OnPanelScaleChanged(SwapChainPanel sender, object args)
+        {
+            if (_graphicsDevice != null) Resized?.Invoke();
+        }
+    }
+}
